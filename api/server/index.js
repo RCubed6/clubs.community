128e4ae812e404f1ab48210c683a1d514422b9f5
NUS16286-11-adaazev <adaazev@NUS16286-11-adaazevs-MacBook-Pro.local>
2022-10-06 08:40:17 -0700

    created backend

---
+++ api/server/index.js
@@ -0,0 +1,11 @@
+// server/index.js
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
