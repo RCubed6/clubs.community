1aa325d2aa7e7511411deca65cc5e24b6543db87
NUS16286-11-adaazev <adaazev@nuevaschool.org>
2022-10-21 21:05:12 -0700

    example of a category button

--- src/App.js
+++ src/App.js
@@ -40,6 +40,16 @@ function App() {
       setFilteredClubs(clubs)
     }
   }
+  {/* For category button */}
+  const handleClick = (event) => {
+    const search = event.target.value
+    if (search) {
+      setFilteredClubs(clubs.filter((club) => 
+      club[7].toLowerCase().includes(search.toLowerCase())))
+    } else {
+      setFilteredClubs(clubs)
+    }
+  }
 
   console.log(filteredClubs)
 
@@ -54,8 +64,11 @@ function App() {
   
 
   return (
-    
     <div className="">
+      {/* Example of a category button; not sure how to integrate */}
+      <button className="categories" onClick={handleClick} value={"Academic"}>
+        Academic
+      </button>
       <input onChange={handleSearch} type="text" placeholder="Search..."/>
         <div class="container">
           {filteredClubs.map((club, index) => (
