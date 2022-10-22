0525f577dfff04d208da105de67fdf982afbe9f0
NUS16286-11-adaazev <adaazev@nuevaschool.org>
2022-10-22 16:16:45 -0700

    integreated more resources and category buttons; very scuffed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './index.css'
 import axios from "axios";
 import React from "react";
 import { render } from '@testing-library/react';
-
 /**
  * Entrypoint component for App 
  */
+
+
+ 
 function App() {
 
   // Component State
@@ -46,7 +48,7 @@ function App() {
       setFilteredClubs(clubs)
     }
   }
-  {/* For category button */}
+  // {/* For category button */}
   const handleClick = (event) => {
     const search = event.target.value
     if (search) {
@@ -57,7 +59,7 @@ function App() {
     }
   }
 
-  console.log(filteredClubs)
+  // console.log(filteredClubs)
 
 // Activates Modal popup
 //
@@ -79,32 +81,61 @@ function App() {
     modal.style.display = "none"
     document.body.style.display = "background-color: rgba(0,0,0,0);"
   }
- 
 
   return (
-
     <div className="">
-      {/* Example of a category button; not sure how to integrate */}
-      <button className="categories" onClick={handleClick} value={"Academic"}>
-        Academic
-      </button>
-      <input onChange={handleSearch} type="text" placeholder="Search..." />
-      <div className="container">
-        {filteredClubs.map((club, index) => (
-          <div key={index} className="clubs" disabled={disable} onClick={createModal}>
-            <div className="modal">
-              <div className="modal_content">
-                <span className="close" onClick={closeModal}>&times;</span>
-                <p>I'm A Pop Up!!!</p>
-              </div>
+      <div className='Main'>
+          <input onChange={handleSearch} type="text" placeholder="Search..." />
+          <div className="container">
+            {filteredClubs.map((club, index) => (
+              <div key={index} className="clubs" disabled={disable} onClick={createModal}>
+                <div className="modal">
+                  <div className="modal_content">
+                    <span className="close" onClick={closeModal}>&times;</span>
+                    <p>I'm A Pop Up!!!</p>
+                  </div>
+                </div>
+
+
+              <h2 className="card-header">{club[2]}</h2>
+              <p className="card-leads">{club[4]}</p>
+              <p className="card-body">{club[6]}</p>
             </div>
-
-
-            <h2 className="card-header">{club[2]}</h2>
-            <p className="card-leads">{club[4]}</p>
-            <p className="card-body">{club[6]}</p>
-          </div>
-        ))}
+          ))}
+        </div>
+      </div>
+      <div className="Categories">
+        <h1>Categories:</h1>
+        <button className="Categories" onClick={handleClick} value={"Academic"}>
+          Academic
+        </button>
+        <button className="Categories"  onClick={handleClick} value={"Activism"}>
+          Activism
+        </button>
+        <button className="Categories"  onClick={handleClick} value={"Affinity Group"}>
+          Affinity Groups
+        </button>
+        <button className="Categories"  onClick={handleClick} value={"Community Service"}>
+          Community Service
+        </button>
+        <button className="Categories"  onClick={handleClick} value={"Hobbies & Interests"}>
+          Hobbies & Interests
+        </button>
+        <button className="Categories"  onClick={handleClick} value={"Language & Culture"}>
+          Language & Culture
+        </button>
+        <button className="Categories"  onClick={handleClick} value={"Sports & Fitness"}>
+          Sports & Fitness
+        </button>
+        <button className="Categories"  onClick={handleClick} value={"STEM"}>
+          STEM
+        </button>
+        <button className="Categories"  onClick={handleClick} value={"Visual & Performing Arts"}>
+          Visual & Performing Arts
+        </button>
+        <button className="Categories"  onClick={handleClick} value={"Writing & Literature"}>
+          Writing & Literature
+        </button>
       </div>
     </div>
   );
