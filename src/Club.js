5d420954477b88caf21cd9568866553e683e0759
Adam Kan <adakan@nus16249-11-adakan.lan>
2022-11-15 20:47:36 -0800

    first commit

---
+++ src/Club.js
@@ -0,0 +1,11 @@
+export class Club {
+    constructor(name, leads, teacher, emails, description, categories) {
+      this.name = name;
+      this.leads = leads;
+      this.teacher = teacher;
+      this.emails = emails;
+      this.description = description;
+      this.categories = categories;
+    }
+  }
+
